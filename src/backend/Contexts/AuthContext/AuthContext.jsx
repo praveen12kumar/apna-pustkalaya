d1b8922a9c97faccef2efb090e5e1ca3565593a4
praveen kumar <praveenshakya1@gmail.com>
2023-06-06 15:32:55 +0530

    commit

--- src/backend/Contexts/AuthContext/AuthContext.jsx
+++ src/backend/Contexts/AuthContext/AuthContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from 'react'
     export const AuthContext = createContext();
 
     export const AuthProvider = ({children})=>{
-    const [isLogIn, setIsLogIn] = useState(localStorage.getItem("encodedToken" != undefined));
+    const [isLogIn, setIsLogIn] = useState(false);
         return(
         <AuthContext.Provider value={{isLogIn, setIsLogIn}}>
             {children}
